c902b555faca2719f30082d80b573ac314ffa18e
Manos Pappas <mano.pappa@gmail.com>
2020-09-15 00:13:14 +0100

    Complete Weather fetch functionality

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -2,13 +2,15 @@ import axios from "axios";
 
 // To signup and get an API KEY
 const URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "tobefetched";
+const API_KEY = "db50ec047b2c0fe3ac030bdd11001f32";
 
 export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
-    q: query,
-    units: "metric",
-    APPID: API_KEY,
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
   });
 
   return data;
